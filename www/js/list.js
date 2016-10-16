2ef5e9dc1a0bf97e7241307c3a47d4729d49ce69
t <y@y.com>
2016-10-16 04:37:27 -0400

    Remove clunky editable

--- www/js/list.js
+++ www/js/list.js
@@ -2,8 +2,6 @@ function initialize() {
 	$("#list li")
 		.wrapInner("<span class='name'>")
 		.append("<span class='initiative'>0</span>");
-		
-	$("span.initiative").editable(function(value, settings) {
-		return(value);
-	}, { type: 'text', submit: 'OK', })
+	
+	
 };
