9791c5baf8b2f5ab08577dbffd2d76c1a484f80e
t <y@y.com>
2016-10-18 10:03:57 -0400

    automatically sort list

--- www/js/list.js
+++ www/js/list.js
@@ -1,7 +1,20 @@
 function initialize() {
-	$("#list li")
-		.wrapInner("<span class='name'>")
-		.append("<span class='initiative'>0</span>");
+    $('li')
+        .wrapInner("<span class='name' contenteditable='true'>")
+		.append("<input class='initiative' type='number'></span>")
 	
-	
+	$('.initiative').change(sortList);
 };
+
+function sortList() {
+	$('#list li').sort(sort_li)
+	             .appendTo('#list');
+}
+
+function sort_li(a, b) {
+	return initValue(a) < initValue(b) ? 1 : -1;
+}
+
+function initValue(a) {
+	return parseInt($(a).find("input.initiative").val());
+}
