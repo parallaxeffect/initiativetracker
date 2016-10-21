fbc5abf5f8e0aadfce9955e871cc72361b9cd7cc
t <y@y.com>
2016-10-21 05:11:22 -0400

    Add table header

--- www/js/list.js
+++ www/js/list.js
@@ -1,4 +1,5 @@
 function initialize() {
+	addHeader();
     addCharacter();
 	$("#addbtn").click(addCharacter);
 	$("#list").on("change", ".initiative", sortList);
@@ -20,13 +21,27 @@ function initValue(a) {
 
 function addCharacter() {
 	var tr = $("<tr />")
-	    .append("<span class='name' contenteditable='true'><br></span>")
-		.append("<input class='initiative' type='number'>")
-		.append("<button class='delete'>x</button>");
+	    .append("<td><span class='name field' contenteditable='true'><br></span></td>")
+		// br fixes firefox display issue
+		.append("<td><input class='initiative' type='number'></td>")
+		.append("<td><span class='AC field' contenteditable='true'><br></span></td>")
+		.append("<td><span class='DC field' contenteditable='true'><br></span></td>")
+		.append("<td><button class='delete'>x</button></td>");
+		
+	$("#list").append(tr);
+}
+
+function addHeader() {
+	var tr = $("<tr class='header' />")
+	    .append("<th class='name'>Character</th>")
+		.append("<th class='init'>Init</th>")
+		.append("<th class='AC'>AC</th>")
+		.append("<th class='DC'>DC</th>")
+		.append("<th>Del</th>");
 		
 	$("#list").append(tr);
 }
 
 function deleteCharacter(e) {
-	$(e.target).parent().remove();
+	$(e.target).parent().parent().remove();
 }
