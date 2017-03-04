5a639492d0e3caffc0e6406db327d657ab9901b1
t <y@y.com>
2017-03-03 19:09:13 -0500

    Add save, load, notes

---
+++ www/js/initiativeView.js
@@ -0,0 +1,138 @@
+
+
+function initializeView() {
+	addHeader();
+	highlightFirst();
+	
+}
+
+function sortList() {
+	$('.list tbody tr').sort(sort_li)
+	             .appendTo('.list tbody');
+}
+
+function sort_li(a, b) {
+	return initValue(a) < initValue(b) ? 1 : -1;
+}
+
+function initValue(a) {
+	return parseInt($(a).find("input.initiative").val());
+}
+
+function getContent(field, value) {
+	if (field.type == "field") {
+		return $("<span contenteditable='true'>").addClass("field").addClass(field.field).data("field", field.field).append(value).append("<br>");
+	}
+	if (field.type == "input") {
+		return $("<input type='number'>").addClass(field.field).val(value);
+	}
+	if (field.type == "delete") {
+		return $("<button title='Delete this character'>").addClass(field.field).html("x");
+	}
+	if (field.type == "copy") {
+		return $("<button title='Copy this character'>").addClass(field.field).html("C");
+	}
+	if (field.type == "notes") {
+		return $("<button title='View Notes'>").addClass(field.field).html("&rarr;");
+	}
+}
+
+function createRow(character, i) {
+	var tr = $("<tr />").data("index", i);
+	
+	$.each(fieldlist, function(i, field) {
+		var td = $("<td>").append(getContent(field, character[field.field]));
+		tr.append(td);
+	});
+	
+	return tr;
+}
+
+function editField(e) {
+	var character = $(e.target).closest("tr");
+	var i = character.data("index");
+	var field = $(e.target).data("field");
+	var value = $(e.target).text();
+	characters[i][field] = value;
+}
+
+function addCharacter(character, i) {
+	
+	var tr = createRow(character, i);	
+	$(".list tbody").append(tr);
+}
+
+function addCharacters(chars) {
+	$.each(chars, function(i, character) {
+		if (character) {
+			var index = characters.push(character)-1;
+			addCharacter(character, index);
+		}
+	});
+}
+
+function newCharacter() {
+	var character = {};
+	var index = characters.push(character) - 1;
+	addCharacter(character, index);
+}
+
+function copyCharacter(e) {
+	var character = $(e.target).closest("tr");
+	character.clone().insertAfter(character);
+}
+
+function addHeader() {
+	var tr = $("<tr class='header' />");
+	$.each(fieldlist, function(i, field) {
+		var th = $("<th>").addClass(field.field)
+		                .html(field.name);
+		tr.append(th);
+	});
+	
+	$(".list thead").append(tr);
+}
+
+function deleteCharacter(e) {
+	var character = $(e.target).closest("tr");
+	var index = character.data("index");
+	delete characters[index];
+	if (character.hasClass("highlight")) {
+		highlightNext();
+	}
+	character.remove();
+}
+
+function selectNotes(e) {
+	var character = $(e.target).closest("tr");
+	var index = character.data("index");
+	
+	$(".notesArea").val(characters[index]["notes"]);
+	notesCharacter = index;
+}
+
+function editNotes() {
+	characters[notesCharacter]["notes"] = $(".notesArea").val();	
+}
+
+function highlightCharacter(c) {
+	c.addClass("highlight");
+}
+
+function highlightFirst() {
+	highlightCharacter($(".list tbody").children().first());
+}
+
+function highlightNext() {
+	var highlight = $(".highlight");
+	highlight.removeClass("highlight");
+
+	var next = highlight.next();
+	if (next.length > 0) {
+		highlightCharacter(next);
+	}
+	else {
+		highlightFirst();
+	}
+}
+
--- www/js/list.js
+++ www/js/list.js
@@ -6,100 +6,66 @@ var fieldlist = [
 	{field: "AC", name: "AC", type: "field"},
 	{field: "DC", name: "DC", type: "field"},
 	{field: "copy", name: "Cpy", type: "copy"},
-	{field: "delete", name: "Del", type: "delete"}	
+	{field: "delete", name: "Del", type: "delete"}, 
+	{field: "notes", name: "Notes", type: "notes"}
 ];
 
 function initialize() {
-	addHeader();
-    addCharacter();
-	highlightFirst();
+	characters = [];
+	
+	initializeView();
+	
+	newCharacter();
+	
 	$(".nextbtn").click(highlightNext);
-	$(".addbtn").click(addCharacter);
+	$(".addbtn").click(newCharacter);
+	$("#file").change(loadFile);
 	$(".list").on("change", ".initiative", sortList);
 	$(".list").on("click", ".delete", deleteCharacter);
 	$(".list").on("click", ".copy", copyCharacter);
+	$(".list").on("click", ".notes", selectNotes);
+	$(".list").on("blur", ".field", editField);
+	$(".savebtn").click(save);
+	$(".notesArea").on("input", editNotes);
 };
 
-function sortList() {
-	$('.list tbody tr').sort(sort_li)
-	             .appendTo('.list tbody');
-}
 
-function sort_li(a, b) {
-	return initValue(a) < initValue(b) ? 1 : -1;
-}
-
-function initValue(a) {
-	return parseInt($(a).find("input.initiative").val());
-}
-
-function getContent(field) {
-	if (field.type == "field") {
-		return $("<span contenteditable='true'>").addClass("field").addClass(field.field).html("<br>");
-	}
-	if (field.type == "input") {
-		return $("<input type='number'>").addClass(field.field);
-	}
-	if (field.type == "delete") {
-		return $("<button title='Delete this character'>").addClass(field.field).html("x");
-	}
-	if (field.type == "copy") {
-		return $("<button title='Copy this character'>").addClass(field.field).html("C");
-	}
-}
-
-function addCharacter() {
-	var tr = $("<tr />");
-	
-	$.each(fieldlist, function(i, field) {
-		var td = $("<td>").append(getContent(field));
-		tr.append(td);
+function save() {
+	var chars = characters.filter(function (c) {
+		return c;
 	});
+	var blob = new Blob([JSON.stringify(chars)], {type: "application/json"});
+	
+	window.saveAs(blob, "my_outfile.json");
+	alert(myjson);
+}
+
+function isCharactersEmpty(chars) {
+	return !chars.some(function (c) { 
+		return !$.isEmptyObject(c);
+	});
+}
+
+function clearCharacters() {
+	debug("Clear Characters");
+}
+
+function loadFile(e) {
+	var files = e.target.files;
+	var reader = new FileReader();
+	
+	if (isCharactersEmpty(characters)) {
+		clearCharacters()
+	}
+	
+	reader.addEventListener("load", function (e) {
+		debug(e.target.result);	
+		addCharacters(JSON.parse(e.target.result));
 		
-	$(".list tbody").append(tr);
-}
-
-function copyCharacter(e) {
-	var character = $(e.target).closest("tr");
-	character.clone().insertAfter(character);
-}
-
-function addHeader() {
-	var tr = $("<tr class='header' />");
-	$.each(fieldlist, function(i, field) {
-		var th = $("<th>").addClass(field.field)
-		                .html(field.name);
-		tr.append(th);
 	});
-	
-	$(".list thead").append(tr);
+	reader.readAsText(files[0]);
 }
 
-function deleteCharacter(e) {
-	var character = $(e.target).closest("tr");
-	if (character.hasClass("highlight")) {
-		highlightNext();
-	}
-	character.remove();
-}
-
-function highlightCharacter(c) {
-	c.addClass("highlight");
-}
-
-function highlightFirst() {
-	highlightCharacter($(".list tbody").children().first());
-}
-
-function highlightNext() {
-	var highlight = $(".highlight");
-	highlight.removeClass("highlight");
-
-	var next = highlight.next();
-	if (next.length > 0) {
-		highlightCharacter(next);
-	}
-	else {
-		highlightFirst();
-	}
+function debug(text) {
+	$(".output").append(text).append("<br>");
 }
